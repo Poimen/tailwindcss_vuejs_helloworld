861a95134557f9e7d14d1e62c87fd2ade19ca58a
Richard Shephard <richards@sahomeloans.com>
2019-04-11 19:53:51 +0200

    Include optimisations

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,17 @@
+const tailwindcss = require('tailwindcss');
+const purgecss = require('@fullhuman/postcss-purgecss');
+const cssnano = require('cssnano');
+const autoprefixer = require('autoprefixer');
+
 module.exports = {
   plugins: [
-    require('tailwindcss')('./tailwind.js'),
-    require('autoprefixer')()
+    tailwindcss('./tailwind.js'),
+    cssnano({
+      preset: 'default'
+    }),
+    purgecss({
+      content: ['./src/**/*.html', './src/**/*.vue']
+    }),
+    autoprefixer
   ]
 };
